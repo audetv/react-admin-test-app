22cbc61d80adef63b323edb7fbca5630ab97ce3e
Aleksey Gusev <audetv@gmail.com>
2021-09-08 15:00:58 +0300

    Customized the Menu Icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './Users';
 import { PostCreate, PostEdit, PostList } from './Posts';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => <Admin dataProvider={dataProvider}>
-  <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-  <Resource name="users" list={UserList} />
+  <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+  <Resource name="users" list={UserList} icon={UserIcon} />
 </Admin>;
 
 export default App;
+
