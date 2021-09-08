77ac0f971a67bd89600671bfda5e3456f79196c3
Aleksey Gusev <audetv@gmail.com>
2021-09-08 12:34:44 +0300

    Added a new <Resource> for the /posts API endpoint using the ListGuesses

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Admin, Resource} from 'react-admin';
+import { Admin, ListGuesser, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './users';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => <Admin dataProvider={dataProvider}>
+  <Resource name="posts" list={ListGuesser} />
   <Resource name="users" list={UserList} />
 </Admin>;
 
