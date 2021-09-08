1a1df9b0d9597f8fc73603712f90eaaa39a80521
Aleksey Gusev <audetv@gmail.com>
2021-09-08 12:44:08 +0300

    Used the username instead id in the ReferenceField

--- src/Posts.tsx
+++ src/Posts.tsx
@@ -5,7 +5,7 @@ export const PostList = (props: ListProps): JSX.Element => (
     <List {...props}>
       <Datagrid rowClick="edit">
         <ReferenceField source="userId" reference="users">
-          <TextField source="id" />
+          <TextField source="name" />
         </ReferenceField>
         <TextField source="id" />
         <TextField source="title" />
