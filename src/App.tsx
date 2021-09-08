ff236e48d66233f3129de7d70cd3b700ea3f9f2f
Aleksey Gusev <audetv@gmail.com>
2021-09-08 12:54:37 +0300

    Used the <EditGuesser> for editing post records.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { Admin, EditGuesser, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './Users';
 import { PostList } from './Posts';
@@ -7,7 +7,7 @@ import { PostList } from './Posts';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => <Admin dataProvider={dataProvider}>
-  <Resource name="posts" list={PostList} />
+  <Resource name="posts" list={PostList} edit={EditGuesser} />
   <Resource name="users" list={UserList} />
 </Admin>;
 
