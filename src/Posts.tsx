473a8b62085eae3b9632f392f83050f67b5c6f34
Aleksey Gusev <audetv@gmail.com>
2021-09-08 12:49:56 +0300

    Changed post datagrid, made the Edit action stand out

--- src/Posts.tsx
+++ src/Posts.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { List, Datagrid, TextField, ReferenceField, ListProps } from 'react-admin';
+import { List, Datagrid, TextField, ReferenceField, ListProps, EditButton } from 'react-admin';
 
 export const PostList = (props: ListProps): JSX.Element => (
     <List {...props}>
-      <Datagrid rowClick="edit">
+      <Datagrid>
+        <TextField source="id" />
         <ReferenceField source="userId" reference="users">
           <TextField source="name" />
         </ReferenceField>
-        <TextField source="id" />
         <TextField source="title" />
-        <TextField source="body" />
+        <EditButton />
       </Datagrid>
     </List>
 );
